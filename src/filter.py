f1a40f8d634ddd39a8cfc61067ca270931ed4f38
Christian D Tuen <christiandt@gmail.com>
2014-03-04 23:14:32 -0800

    Renamed test-filed and fixed variable-types

--- src/filter.py
+++ src/filter.py
@@ -9,39 +9,49 @@ class Filter():
 		self.words = dict()
 
 
+	def trim_word(self, word):
+		word = word.strip(' .:,-!()"?+<>*')
+		word = word.lower()
+		return word
+
+
 	def train(self, file_name):
 		lineNumber = 1
 		ham_words = 0
 		spam_words = 0
 
+		# Loop through all the lines
 		for line in file_name:
 			if lineNumber % 2 != 0:
 				line = line.split('\t')
 				category = line[0]
 				input_words = line[1].strip().split(' ')
-				for input_word in input_words:
-					input_word = input_word.strip(' .:,-!()"?+<>*')
-					input_word = input_word.lower()
-					if input_word != "":
 
+				#Loop through all the words in the line, remove some characters
+				for input_word in input_words:
+					input_word = input_word
+					if input_word != "":
+						input_word = self.trim_word(input_word)
+						# Check if word is in dicionary, else add
 						if input_word in self.words:
 							word = self.words[input_word]
 						else:
 							word = Word(input_word)
 							self.words[input_word] = word
 
+						# Check wether the word is in ham or spam sentence, increment counters
 						if category == "ham":
 							word.increment_ham()
 							ham_words += 1
 						elif category == "spam":
 							word.increment_spam()
 							spam_words += 1
+
+						# Probably bad input...
 						else:
 							print "Not valid"
 						
 			lineNumber+=1
-		#print ham_words
-		#print spam_words
 
 		for word in self.words:
 			self.words[word].compute_probability(ham_words, spam_words)
@@ -51,9 +61,39 @@ class Filter():
 			#print self.words[word].spam_count
 			#print self.words[word].get_probability()
 
+	def get_intresting_words(self, sms):
+		intresting_words = []
+
+		for input_word in sms.split(' '):
+			input_word = self.trim_word(input_word)
+			if input_word != "":
+				if input_word in self.words:
+					word = self.words[input_word]
+				else:
+					word = Word(input_word)
+					word.set_probability(0.40)
+				intresting_words.append(word)
+
+		intresting_words.sort(key=lambda word: word.interesting(), reverse=True)
+		return intresting_words[0:15]
 
 
-train_file = open("smsSpamCollection-train.txt", "rb")
+train = sys.argv[1]
+messages = sys.argv[2]
+
+train_file = open(train, "rb")
+messages_file = open(messages, "rb")
 
 f = Filter()
 f.train(train_file)
+
+for sms in messages_file:
+	print "*********************"
+	print sms
+	for word in f.get_intresting_words(sms):
+		print word.get_word()
+	print "*********************"
+
+
+train_file.close()
+messages_file.close()
--- src/word.py
+++ src/word.py
@@ -2,8 +2,8 @@ class Word():
 
 	def __init__(self, word):
 		self.word = word;
-		self.spam_count = 0
-		self.ham_count = 0
+		self.spam_count = 0.0
+		self.ham_count = 0.0
 		self.spam_fraction = 0.0
 		self.ham_fraction = 0.0
 		self.spam_probability = 0.0
@@ -12,26 +12,28 @@ class Word():
 		self.spam_fraction = float(self.spam_count) / float(spam_total)
 		self.ham_fraction = float(self.ham_count) / float(ham_total)
 
-		print str(self.spam_count) + " / " + str(spam_total) + " = " + str(self.spam_fraction)
-
-		#print self.ham_fraction
-		#print self.spam_fraction
-
 		if self.ham_fraction + self.spam_fraction > 0:
 			self.spam_probability = self.spam_fraction / (self.spam_fraction + self.ham_fraction)
-		#if self.spam_probability < 0.01:
-		#	self.spam_probability = 0.01
-		#elif self.spam_probability > 0.99:
-		#	self.spam_probability = 0.99
+		if self.spam_probability < 0.01:
+			self.spam_probability = 0.01
+		elif self.spam_probability > 0.99:
+			self.spam_probability = 0.99
+
+	def set_probability(self, probability):
+		self.spam_probability = probability
 
 	def interesting(self):
-		return Math.abs(0.5-self.spam_probability)
+		return abs(0.5-self.spam_probability)
 
 	def increment_ham(self):
-		self.ham_count += 1
+		self.ham_count += 1.0
 
 	def increment_spam(self):
-		self.spam_count += 1
+		self.spam_count += 1.0
 
 	def get_probability(self):
 		return self.spam_probability
+
+	def get_word(self):
+		return self.word
+
