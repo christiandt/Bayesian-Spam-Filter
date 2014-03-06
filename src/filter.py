e758f44dd07d1e76b7548a9a3a574e2834518432
Christian D Tuen <christiandt@gmail.com>
2014-03-06 15:25:30 -0800

    Moved filter-logic from main to filter where it belongs

--- src/filter.py
+++ src/filter.py
@@ -14,13 +14,13 @@ class Filter():
 		return word
 
 
-	def train(self, file_name):
+	def train(self, train_file):
 		lineNumber = 1
 		ham_words = 0
 		spam_words = 0
 
 		# Loop through all the lines
-		for line in file_name:
+		for line in train_file:
 			if lineNumber % 2 != 0:
 				line = line.split('\t')
 				category = line[0]
@@ -28,9 +28,8 @@ class Filter():
 
 				#Loop through all the words in the line, remove some characters
 				for input_word in input_words:
-					input_word = input_word
+					input_word = self.trim_word(input_word)
 					if input_word != "":
-						input_word = self.trim_word(input_word)
 
 						# Check if word is in dicionary, else add
 						if input_word in self.words:
@@ -57,8 +56,9 @@ class Filter():
 		for word in self.words:
 			self.words[word].compute_probability(ham_words, spam_words)
 
-	def get_intresting_words(self, sms):
-		intresting_words = []
+
+	def get_interesting_words(self, sms):
+		interesting_words = []
 
 		# Go through all words in the SMS and append to list. 
 		# If we have not seen the word in training, assign probability of 0.4
@@ -70,8 +70,33 @@ class Filter():
 				else:
 					word = Word(input_word)
 					word.set_probability(0.40)
-				intresting_words.append(word)
+				interesting_words.append(word)
 
 		# Sort the list of interesting words, return top 15 elements if list is longer than 15
-		intresting_words.sort(key=lambda word: word.interesting(), reverse=True)
-		return intresting_words[0:15]
+		interesting_words.sort(key=lambda word: word.interesting(), reverse=True)
+		return interesting_words[0:15]
+
+
+	def filter(self, input_file, result_file):
+		# Loop through all SMSes and compute total spam probability of the sms-message
+		lineNumber = 0
+		for sms in input_file:
+			lineNumber+=1
+			spam_product = 1.0
+			ham_product = 1.0
+			if lineNumber % 2 != 0:
+				try:
+					for word in self.get_interesting_words(sms):
+						spam_product *= word.get_probability()
+						ham_product *= (1.0 - word.get_probability())
+
+					sms_spam_probability = spam_product / (spam_product + ham_product)
+				except:
+					result_file.write("error")
+					
+				if sms_spam_probability > 0.9:
+					result_file.write("spam")
+				else:
+					result_file.write("ham")
+			result_file.write("\n")
+
--- src/main.py
+++ src/main.py
@@ -14,28 +14,8 @@ predictions_file = open("predictions.txt", "w")
 f = Filter()
 f.train(train_file)
 
-# Loop through all SMSes and compute total spam probability of the sms-message
-lineNumber = 0
-for sms in messages_file:
-	lineNumber+=1
-	spam_product = 1.0
-	ham_product = 1.0
-	if lineNumber % 2 != 0:
-		try:
-			for word in f.get_intresting_words(sms):
-				spam_product *= word.get_probability()
-				ham_product *= (1.0 - word.get_probability())
-
-			sms_spam_probability = spam_product / (spam_product + ham_product)
-		except:
-			predictions_file.write("error")
-			
-		if sms_spam_probability > 0.9:
-			predictions_file.write("spam")
-		else:
-			predictions_file.write("ham")
-	predictions_file.write("\n")
-
+#filter the messages in messages_file, write results to predictions_file
+f.filter(messages_file, predictions_file)
 
 # Close all the files
 train_file.close()
